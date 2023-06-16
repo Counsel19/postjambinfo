b04c97d91dfa7b267529917294e9cab34eaf8e16
Okpabi Counsel <okpabicounsel@gmail.com>
2023-06-16 04:05:30 +0100

    ui z-index

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
 
               {showDesktopDD && (
                 <div
-                  className=" absolute right-0 z-10 mt-1 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  className=" absolute right-0 z-50 mt-1 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                   role="menu"
                   aria-orientation="vertical"
                   aria-labelledby="menu-button"
