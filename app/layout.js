4f6a6c44f112715286950588bd11d2bf24dedbec
Okpabi Counsel <okpabicounsel@gmail.com>
2023-06-16 04:49:15 +0100

    finishing

--- app/layout.js
+++ app/layout.js
@@ -7,14 +7,14 @@ import AppContextProvider from "@/context/AppContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Post Jamb SMS Notification",
+  title: "Post UTME SMS Notification",
   description:
-    "Stay informed with latest update from your choosen universities",
+    "We can send  you latest information via sms to your phone sharp sharp as it is happening in your   Jamb choice school so that this year 2023 you will not miss your admission",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ scrollBehavior: "smooth" }}>
       <body className={inter.className}>
         <AppContextProvider>
           <Navbar />
